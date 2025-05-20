fe598b5ed3c0488029b26e8dbec87f365b7fd456
Max Ryan <161385834+maxryan4@users.noreply.github.com>
2025-05-20 16:16:46 +0100

    Added Mandelbrot Fractal Core and testbench

---
+++ Hardware/rtl/mandelbrotCore.sv
@@ -0,0 +1,89 @@
+module mandelbrotCore#(
+    DATA_WIDTH = 32,
+    MAX_ITER_WIDTH = 16
+)(
+    input logic clk_i,
+    input logic rst_i,
+    input logic start_i,
+    input logic signed [DATA_WIDTH-1:0] x0_i, // c = x0 + i * y0 using Q16.16 representation
+    input logic signed [DATA_WIDTH-1:0] y0_i,
+    input logic [MAX_ITER_WIDTH-1:0] max_iter_i,
+
+    output logic [MAX_ITER_WIDTH-1:0] iter_o,
+    output logic done_o
+);
+
+logic signed [DATA_WIDTH-1:0] x, y; // z = x + i * y
+logic signed [DATA_WIDTH-1:0] x2, y2, xy; // x^2, y^2, x*y
+logic [MAX_ITER_WIDTH-1:0] iter;
+logic running;
+
+qMult #( 
+    .FRACTIONAL_BITS(16), 
+    .INTEGER_BITS(16)
+) qMult_x2(
+    .input1_i(x),
+    .input2_i(x),
+    .result_o(x2)
+);
+
+qMult #( 
+    .FRACTIONAL_BITS(16), 
+    .INTEGER_BITS(16)
+) qMult_y2(
+    .input1_i(y),
+    .input2_i(y),
+    .result_o(y2)
+);
+
+qMult #( 
+    .FRACTIONAL_BITS(16), 
+    .INTEGER_BITS(16)
+) qMult_xy(
+    .input1_i(x),
+    .input2_i(y),
+    .result_o(xy)
+);
+
+always_ff @(posedge clk_i or posedge rst_i) begin
+    if (rst_i) begin
+        x <= 0;
+        y <= 0;
+        iter <= 0;
+        done_o <= 0;
+        iter_o <= 0;
+        running <= 0;
+    end
+
+    else begin
+        if (start_i && !running) begin
+            x <= x0_i;
+            y <= y0_i;
+            iter <= 0;
+            done_o <= 0;
+            running <= 1;
+        end
+        else if (running) begin
+            // Check if |z|^2 > 4
+            if (x2 + y2 > 4 << DATA_WIDTH/2) begin
+                done_o <= 1;
+                iter_o <= iter + 1; // i think the plus 1 is correct as are checking the previous cycles values
+                running <= 0;
+            end
+            else if (iter < max_iter_i) begin
+                iter <= iter + 1;
+                x <= x2 - y2 + x0_i; // z = z^2 + c
+                y <= (xy <<< 1) + y0_i; // z = z^2 + c
+            end
+            else begin
+                done_o <= 1;
+                iter_o <= iter;
+                running <= 0;
+            end
+
+        end
+    end
+
+end
+
+endmodule
---
+++ Hardware/rtl/qMult.sv
@@ -0,0 +1,24 @@
+module qMult #(parameter
+    INTEGER_BITS = 8,
+    FRACTIONAL_BITS = 24
+) (
+    input logic signed [DATA_WIDTH-1:0] input1_i,
+    input logic signed [DATA_WIDTH-1:0] input2_i,
+    output logic signed [DATA_WIDTH-1:0] result_o
+);
+
+localparam int DATA_WIDTH = INTEGER_BITS + FRACTIONAL_BITS;
+
+localparam logic [2*DATA_WIDTH-1:0] ROUNDING_OFFSET = 1 << (FRACTIONAL_BITS - 1);
+
+/* verilator lint_off UNUSEDSIGNAL */
+logic signed [DATA_WIDTH*2-1:0] mult_result;
+/* verilator lint_on UNUSEDSIGNAL */
+
+always_comb begin
+    mult_result = input1_i * input2_i;
+    mult_result = mult_result + ROUNDING_OFFSET;  // round to nearest
+    result_o = mult_result[DATA_WIDTH + FRACTIONAL_BITS -1 : FRACTIONAL_BITS];  // slice bits
+end
+    
+endmodule
